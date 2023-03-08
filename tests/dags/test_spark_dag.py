235604e69f4dffe1f54016f283c90eb99d5e8b04
Le Cong Huy <huy.le@robolee.local>
2023-03-09 00:18:05 +0700

    Update test_spark_dag.py to use SparkSubmitOperator

--- tests/dags/test_spark_dag.py
+++ tests/dags/test_spark_dag.py
@@ -25,8 +25,8 @@ import os
 from datetime import datetime
 
 from airflow.models import DAG
-from airflow.providers.apache.spark.operators.spark_jdbc import SparkJDBCOperator
-from airflow.providers.apache.spark.operators.spark_sql import SparkSqlOperator
+# from airflow.providers.apache.spark.operators.spark_jdbc import SparkJDBCOperator
+# from airflow.providers.apache.spark.operators.spark_sql import SparkSqlOperator
 from airflow.providers.apache.spark.operators.spark_submit import SparkSubmitOperator
 
 ENV_ID = os.environ.get("SYSTEM_TESTS_ENV_ID")
@@ -45,34 +45,34 @@ with DAG(
     )
     # [END howto_operator_spark_submit]
 
-    # [START howto_operator_spark_jdbc]
-    jdbc_to_spark_job = SparkJDBCOperator(
-        cmd_type="jdbc_to_spark",
-        jdbc_table="foo",
-        spark_jars="${SPARK_HOME}/jars/postgresql-42.2.12.jar",
-        jdbc_driver="org.postgresql.Driver",
-        metastore_table="bar",
-        save_mode="overwrite",
-        save_format="JSON",
-        task_id="jdbc_to_spark_job",
-    )
+    # # [START howto_operator_spark_jdbc]
+    # jdbc_to_spark_job = SparkJDBCOperator(
+        # cmd_type="jdbc_to_spark",
+        # jdbc_table="foo",
+        # spark_jars="${SPARK_HOME}/jars/postgresql-42.2.12.jar",
+        # jdbc_driver="org.postgresql.Driver",
+        # metastore_table="bar",
+        # save_mode="overwrite",
+        # save_format="JSON",
+        # task_id="jdbc_to_spark_job",
+    # )
 
-    spark_to_jdbc_job = SparkJDBCOperator(
-        cmd_type="spark_to_jdbc",
-        jdbc_table="foo",
-        spark_jars="${SPARK_HOME}/jars/postgresql-42.2.12.jar",
-        jdbc_driver="org.postgresql.Driver",
-        metastore_table="bar",
-        save_mode="append",
-        task_id="spark_to_jdbc_job",
-    )
-    # [END howto_operator_spark_jdbc]
+    # spark_to_jdbc_job = SparkJDBCOperator(
+        # cmd_type="spark_to_jdbc",
+        # jdbc_table="foo",
+        # spark_jars="${SPARK_HOME}/jars/postgresql-42.2.12.jar",
+        # jdbc_driver="org.postgresql.Driver",
+        # metastore_table="bar",
+        # save_mode="append",
+        # task_id="spark_to_jdbc_job",
+    # )
+    # # [END howto_operator_spark_jdbc]
 
-    # [START howto_operator_spark_sql]
-    spark_sql_job = SparkSqlOperator(
-        sql="SELECT COUNT(1) as cnt FROM temp_table", master="local", task_id="spark_sql_job"
-    )
-    # [END howto_operator_spark_sql]
+    # # [START howto_operator_spark_sql]
+    # spark_sql_job = SparkSqlOperator(
+        # sql="SELECT COUNT(1) as cnt FROM temp_table", master="local", task_id="spark_sql_job"
+    # )
+    # # [END howto_operator_spark_sql]
 
 from tests.system.utils import get_test_run  # noqa: E402
 
